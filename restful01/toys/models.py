0ba754ff7be8121c49d4704c326462f70e62c3cd
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2024-07-29 23:54:30 -0300

    feat: create toy model and migrations

--- restful01/toys/models.py
+++ restful01/toys/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Toy(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250, blank=True, default="")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=50)
+    was_included_in_home = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Meta:
+    ordering = ("name",)
