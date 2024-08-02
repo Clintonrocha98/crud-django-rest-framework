25c4c7b819c108d07e62ef24509d675a9b4af835
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2024-08-02 07:29:35 -0300

    update: change toy model fields and migrations

--- restful01/toys/models.py
+++ restful01/toys/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 
 class Toy(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=250, blank=True, default="")
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.CharField(max_length=50)
+    name = models.CharField(max_length=100, blank=False)
+    description = models.CharField(max_length=250, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
+    category = models.CharField(max_length=50, blank=False)
     was_included_in_home = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
-
-class Meta:
-    ordering = ("name",)
+    class Meta:
+        ordering = ["name"]
